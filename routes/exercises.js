f7c07215872b9dbb983e40c07e2ee59db1ef6965
tieutac1996 <vietkaka1996@gmail.com>
2020-06-22 20:59:19 +0700

    'd'

--- routes/exercises.js
+++ routes/exercises.js
@@ -10,9 +10,7 @@ router.route('/').get((req, res) => {
   //   }
   // })
   Exercise.find()
-    .then((exercises) => {
-      res.json(exercises);
-    })
+    .then((exercises) => res.json(exercises))
     .catch((err) => res.status(400).json('Error: ' + err));
 });
 
