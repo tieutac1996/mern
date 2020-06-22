2c89c5de413ba10a786816caf5ef272d9aa3eb9e
tieutac1996 <vietkaka1996@gmail.com>
2020-06-23 00:35:21 +0700

    'aaa'

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
@@ -19,8 +17,8 @@ mongoose.connect(uri, {
 });
 const connection = mongoose.connection;
 
-connection.once('open', () => {
-  console.log('MongoDB database ');
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected!!!!');
 });
 
 const exercisesRouter = require('./routes/exercises');
